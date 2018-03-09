20c0d8fbad6039f62247b8812efd50ac2188c546
ArthurGoodman <arthur.goodman3@gmail.com>
2018-03-09 21:51:58 +0200

    Implemented basic console functionality

--- include/qconsole/QConsole.hpp
+++ include/qconsole/QConsole.hpp
@@ -1,5 +1,57 @@
 #pragma once
 
+#include <cstddef>
+#include <functional>
+#include <string>
+#include <vector>
+#include <QtWidgets/QPlainTextEdit>
+
 namespace qconsole {
+
+class QConsole : public QPlainTextEdit
+{
+    Q_OBJECT
+
+public: // methods
+    QConsole(QWidget *parent = nullptr);
+
+    void setProcesor(const std::function<void(const std::string &)> &processor);
+
+    void setPrompt(const std::string &prompt);
+
+    void lock();
+    void unlock();
+
+    void write(const std::string &str);
+    QConsole &operator<<(const std::string &str);
+
+    void insertBlock();
+    void insertPrompt();
+
+    void setBaseColor(const QColor &color);
+    void setTextColor(const QColor &color);
+
+protected: // methods
+    void keyPressEvent(QKeyEvent *e) override;
+    void mousePressEvent(QMouseEvent *e) override;
+    void mouseDoubleClickEvent(QMouseEvent *e) override;
+    void contextMenuEvent(QContextMenuEvent *e) override;
+
+private: // methods
+    void onReturn();
+    void historyAdd(const std::string &command);
+    void historyBack();
+    void historyForward();
+    void scrollDown();
+
+private: // fields
+    std::function<void(const std::string &)> m_processor;
+
+    std::vector<std::string> m_history;
+    std::string m_prompt;
+
+    std::size_t m_history_pos;
+    bool m_locked;
+};
 
 } // namespace qconsole
--- src/QConsole.cpp
+++ src/QConsole.cpp
@@ -1,5 +1,295 @@
 #include "qconsole/QConsole.hpp"
+#include <QtWidgets/QtWidgets>
 
 namespace qconsole {
+
+QConsole::QConsole(QWidget *parent)
+    : QPlainTextEdit{parent}
+    , m_history_pos{0}
+    , m_locked{false}
+{
+    setBaseColor(QColor(40, 40, 40));
+    setTextColor(QColor(230, 230, 230));
+
+    setFrameShape(QFrame::NoFrame);
+    viewport()->setCursor(Qt::ArrowCursor);
+
+    setWordWrapMode(QTextOption::WrapAnywhere);
+    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+
+    setCursorWidth(2);
+}
+
+void QConsole::setProcesor(
+    const std::function<void(const std::string &)> &processor)
+{
+    m_processor = processor;
+}
+
+void QConsole::setPrompt(const std::string &prompt)
+{
+    m_prompt = prompt;
+    insertPrompt();
+}
+
+void QConsole::lock()
+{
+    m_locked = true;
+}
+
+void QConsole::unlock()
+{
+    m_locked = false;
+}
+
+void QConsole::write(const std::string &str)
+{
+    textCursor().insertBlock();
+    insertPlainText(str.c_str());
+    scrollDown();
+}
+
+QConsole &QConsole::operator<<(const std::string &str)
+{
+    write(str);
+    return *this;
+}
+
+void QConsole::insertBlock()
+{
+    textCursor().insertBlock();
+}
+
+void QConsole::insertPrompt()
+{
+    insertPlainText(m_prompt.c_str());
+    scrollDown();
+    m_locked = false;
+}
+
+void QConsole::setBaseColor(const QColor &c)
+{
+    QPalette p = palette();
+    p.setColor(QPalette::Base, c);
+    setPalette(p);
+}
+
+void QConsole::setTextColor(const QColor &c)
+{
+    QPalette p = palette();
+    p.setColor(QPalette::Text, c);
+    setPalette(p);
+}
+
+void QConsole::keyPressEvent(QKeyEvent *e)
+{
+    ///@todo add selections
+
+    if (m_locked)
+    {
+        return;
+    }
+
+    if ((e->key() == Qt::Key_V && e->modifiers() == Qt::ControlModifier))
+    {
+        QPlainTextEdit::keyPressEvent(e);
+    }
+    else if (
+        e->key() >= Qt::Key_Space && e->key() <= Qt::Key_BraceRight &&
+        e->key() != Qt::Key_QuoteLeft)
+    {
+        if (e->modifiers() == Qt::NoModifier ||
+            e->modifiers() == Qt::ShiftModifier)
+        {
+            QPlainTextEdit::keyPressEvent(e);
+        }
+    }
+    else
+    {
+        switch (e->key())
+        {
+        case Qt::Key_Return:
+            onReturn();
+            break;
+
+        case Qt::Key_Up:
+            historyBack();
+            break;
+        case Qt::Key_Down:
+            historyForward();
+            break;
+
+        case Qt::Key_Backspace:
+            if (textCursor().positionInBlock() >
+                static_cast<int>(m_prompt.size()))
+            {
+                QPlainTextEdit::keyPressEvent(e);
+            }
+            break;
+
+        case Qt::Key_End:
+        case Qt::Key_Delete:
+            QPlainTextEdit::keyPressEvent(e);
+            break;
+
+        case Qt::Key_Left:
+            if (e->modifiers() == Qt::NoModifier)
+            {
+                QPlainTextEdit::keyPressEvent(e);
+
+                QTextCursor cursor = textCursor();
+
+                if (cursor.positionInBlock() <
+                    static_cast<int>(m_prompt.size()))
+                {
+                    cursor.movePosition(
+                        QTextCursor::Right, QTextCursor::MoveAnchor);
+                }
+
+                setTextCursor(cursor);
+            }
+            else if (e->modifiers() == Qt::ControlModifier)
+            {
+                QPlainTextEdit::keyPressEvent(e);
+
+                QTextCursor cursor = textCursor();
+
+                if (cursor.positionInBlock() <
+                    static_cast<int>(m_prompt.size()))
+                {
+                    cursor.movePosition(
+                        QTextCursor::Right, QTextCursor::MoveAnchor, 2);
+                }
+
+                setTextCursor(cursor);
+            }
+            break;
+
+        case Qt::Key_Right:
+            if (e->modifiers() == Qt::NoModifier ||
+                e->modifiers() == Qt::ControlModifier)
+            {
+                QPlainTextEdit::keyPressEvent(e);
+            }
+            break;
+
+        case Qt::Key_Home:
+        {
+            QTextCursor cursor = textCursor();
+            cursor.movePosition(QTextCursor::StartOfBlock);
+            cursor.movePosition(
+                QTextCursor::Right, QTextCursor::MoveAnchor, m_prompt.size());
+            setTextCursor(cursor);
+        }
+        break;
+
+        case Qt::Key_PageUp:
+        {
+            QScrollBar *vbar = verticalScrollBar();
+            vbar->setValue(vbar->value() - 20);
+        }
+        break;
+
+        case Qt::Key_PageDown:
+        {
+            QScrollBar *vbar = verticalScrollBar();
+            vbar->setValue(vbar->value() + 20);
+        }
+        break;
+
+        default:
+            QWidget::keyPressEvent(e);
+        }
+    }
+}
+
+void QConsole::mousePressEvent(QMouseEvent *)
+{
+    setFocus();
+}
+
+void QConsole::mouseDoubleClickEvent(QMouseEvent *)
+{
+}
+
+void QConsole::contextMenuEvent(QContextMenuEvent *)
+{
+}
+
+void QConsole::onReturn()
+{
+    QTextCursor cursor = textCursor();
+    cursor.movePosition(QTextCursor::EndOfBlock);
+    setTextCursor(cursor);
+
+    std::string command =
+        cursor.block().text().mid(m_prompt.size()).toStdString();
+    historyAdd(command);
+
+    if (m_processor)
+    {
+        m_processor(command);
+    }
+
+    insertBlock();
+    insertPrompt();
+}
+
+void QConsole::scrollDown()
+{
+    QScrollBar *vbar = verticalScrollBar();
+    vbar->setValue(vbar->maximum());
+}
+
+void QConsole::historyAdd(const std::string &command)
+{
+    m_history.emplace_back(command);
+    m_history_pos = m_history.size();
+}
+
+void QConsole::historyBack()
+{
+    if (!m_history_pos)
+    {
+        return;
+    }
+
+    QTextCursor cursor = textCursor();
+    cursor.movePosition(QTextCursor::StartOfBlock);
+    cursor.movePosition(QTextCursor::EndOfBlock, QTextCursor::KeepAnchor);
+    cursor.removeSelectedText();
+
+    setTextCursor(cursor);
+
+    insertPlainText((m_prompt + m_history.at(m_history_pos - 1)).c_str());
+
+    m_history_pos--;
+}
+
+void QConsole::historyForward()
+{
+    if (m_history_pos == m_history.size())
+    {
+        return;
+    }
+
+    QTextCursor cursor = textCursor();
+    cursor.movePosition(QTextCursor::StartOfBlock);
+    cursor.movePosition(QTextCursor::EndOfBlock, QTextCursor::KeepAnchor);
+    cursor.removeSelectedText();
+
+    setTextCursor(cursor);
+
+    if (m_history_pos == m_history.size() - 1)
+    {
+        insertPlainText(m_prompt.c_str());
+    }
+    else
+    {
+        insertPlainText((m_prompt + m_history.at(m_history_pos + 1)).c_str());
+    }
+
+    m_history_pos++;
+}
 
 } // namespace qconsole
